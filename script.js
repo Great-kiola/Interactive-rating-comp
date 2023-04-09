18133bceac88b0b0e1d5af11284321f270a945fe
Ola <hollabodey.kilaso@gmail.com>
2023-04-09 18:49:35 +0100

    Made changes to the readme file

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Connected successfully!");
+let myRate = document.querySelectorAll(".rate");
+let rateBox = document.querySelector("#rating");
+
+
+$("#submit").click(function(e){
+    e.preventDefault();
+    $("#thanks").removeClass('hide');
+    $(".container").addClass('hide');
+
+})
+
+myRate.forEach((rate) => {
+    rate.addEventListener("click", () => {
+        rateBox.innerHTML = rate.innerHTML;
+    });
+})
